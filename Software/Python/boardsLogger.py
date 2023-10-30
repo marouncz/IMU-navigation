9465bb867ed318b29417c0390daaf3937c1283a0
marouncz <marouncz@gmail.com>
2023-10-30 23:09:06 +0100

    Correct UINT32 manipulation and convertion of gyro and accel data to SI units

--- Software/Python/boardsLogger.py
+++ Software/Python/boardsLogger.py
@@ -11,8 +11,21 @@ def dmm_to_dd(dmm_str, direction):
         dd = -dd
     return dd
 
+def twos_complement_to_int(value):
+    # Check if the most significant bit (sign bit) is set
+    if value & 0x80000000:
+        # Negative value
+        # Extract the magnitude (least significant 31 bits) and subtract it from 2^31
+        magnitude = value & 0x7FFFFFFF
+        integer_value = -(2**31 - magnitude)
+    else:
+        # Positive value, use the magnitude as is
+        integer_value = value
+
+    return integer_value
+
 # Define the serial port and baud rate
-port = 'COM27'
+port = 'COM19'
 baud_rate = 9600
 
 # Open the serial port
@@ -28,6 +41,7 @@ import os
 import System
 import csv
 import datetime;
+import math
 
 # open the file in the write mode
 f = open('boardsLoggerData.csv', 'w')
@@ -62,14 +76,20 @@ sleep(0.5)
 
 #Create reg list
 
-regs_py = ['MSC_CTRL','DATA_CNTR','X_GYRO_OUT','Y_GYRO_OUT','Z_GYRO_OUT','X_ACCL_OUT','Y_ACCL_OUT','Z_ACCL_OUT']
-csvHeader = regs_py + ["LATITUDE", "LONGITUDE", "ALTITUDE"]
+regs_py = ['MSC_CTRL','DATA_CNTR','X_GYRO_LOW','Y_GYRO_LOW','Z_GYRO_LOW','X_ACCL_LOW','Y_ACCL_LOW','Z_ACCL_LOW']
+
+LSBtoms2 = 2.45e-3/(2**16)
+LSBtorads = math.radians(0.025/(2**16))
+
+
+gpsRegs = ["LATITUDE", "LONGITUDE", "ALTITUDE"]
+csvHeader = regs_py + gpsRegs
 writer.writerow(csvHeader)
 regs = Array[String](regs_py)
 data = []
 previousData = [0, 0]
 Dut.StartBufferedStream(regs, System.UInt32(1), System.UInt32(20000), System.Int32(10))
-for i in range(1, 100):
+for i in range(1, 500):
     # data = Dut.ReadSigned(regs, System.UInt32(10), System.UInt32(10))
     sleep(0.1)
     buffer = Dut.GetBufferedStreamDataPacket()
@@ -77,8 +97,17 @@ for i in range(1, 100):
     while buffer is not None:
         data = Dut.ConvertBufferDataToU32(buffer)
         if data[1] != previousData[1]:
-            writer.writerow(data)
-            #print(data[1])
+            intData = [int(value) for value in data]
+            # writer.writerow(data)
+            intData[2] = twos_complement_to_int(intData[2])*LSBtorads
+            intData[3] = twos_complement_to_int(intData[3])*LSBtorads
+            intData[4] = twos_complement_to_int(intData[4])*LSBtorads
+            intData[5] = twos_complement_to_int(intData[5])*LSBtoms2
+            intData[6] = twos_complement_to_int(intData[6])*LSBtoms2
+            intData[7] = twos_complement_to_int(intData[7])*LSBtoms2
+
+            writer.writerow(intData + ['']*len(gpsRegs))
+            # print(data[1])
             previousData = data
         buffer = Dut.GetBufferedStreamDataPacket()
     #Dut.StartBufferedStream(regs, System.UInt32(1), System.UInt32(2000), System.Int32(10))
@@ -90,7 +119,7 @@ for i in range(1, 100):
     # Check if the line contains GPS data
     if line.startswith('$GNGGA'):
         data = line.split(',')
-        if len(data) >= 10:
+        if len(data) >= 10 and not(data[2] == ''):
             latitude = dmm_to_dd(data[2], data[3])
             longitude = dmm_to_dd(data[4], data[5])
             altitude = data[9]
